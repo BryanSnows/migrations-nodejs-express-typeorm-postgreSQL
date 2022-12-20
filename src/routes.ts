5bb93c99ed36c2d3588fc87a687d93d470e71447
bryan neves pinto <bryan.pinto@conecthus.org.br>
2022-12-20 17:18:28 -0400

    feat: create endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controller/CreateCategoryController";
+import { CreateVideoController } from "./controller/CreateVideoController";
 import { DeleteCategoryController } from "./controller/DeleteCategoryController";
 import { GetAllCategoryController } from "./controller/GetAllCategoryController";
 import { UpdateCategoryController } from "./controller/UpdateCategoryController";
@@ -10,5 +11,8 @@ routes.post("/categories", new CreateCategoryController().handle)
 routes.get("/categories", new GetAllCategoryController().handle)
 routes.delete("/categories/:id", new DeleteCategoryController().handle)
 routes.put("/categories/:id", new UpdateCategoryController().handle)
+
+
+routes.post("/videos", new CreateVideoController().handle)
 
 export {routes};
